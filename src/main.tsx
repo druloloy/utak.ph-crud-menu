788630479fcb5ac995b42fa09dcbb998b8000daf
Andrew Loloy <druloloy>
2024-06-12 22:12:26 +0800

    added support for options price, cost, and stock values

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+if (import.meta.env.PROD) {
+	// remove console logs in production
+	console.log = () => {};
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
